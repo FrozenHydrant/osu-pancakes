3345a88a7d951ffcea5256f1435cd2447afb011b
WaterHydrant <richard.you.777@gmail.com>
2026-04-12 19:39:25 -0700

    Project structure fixes

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
 from flask import Flask
 
-class App:
-    
-    my_app: Flask = Flask(__name__)
-    instance = None
-    
 
-    def __init__(self):
-        if App.instance is None:
-            App.instance = self
-        else:
-            raise Exception("Can't have more than one app.")
+# Setup
+app: Flask = Flask(__name__)
 
 
-    @my_app.route("/")
-    def read_root():
-        return "Hello World"
-        
-    
-    def start(self):
-        App.my_app.run()
+@app.route("/")
+def read_root():
+    return "Hello World"
 
 
 if __name__ == "__main__":
-    app = App() # Need initialization
-    app.start()
+    app.run()
